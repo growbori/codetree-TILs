f1005c6e7101d9454dd28a552cd50cfe142b9eb0
growbori <156386929+growbori@users.noreply.github.com>
2024-01-14 20:19:11 +0900

    Title: 특정 조건 배수의 합, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240114/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
+++ 240114/특정 조건 배수의 합/sum-of-specific-condition-multiple.py
@@ -1,7 +1,16 @@
 a, b = map(int, input().split())
-count = 0
-for i in range(a, b+1):
-    if i % 5 == 0:
-        count += i
 
-print(count)
+count = 0
+if a < b:
+    for i in range(a, b+1):
+        if i % 5 == 0:
+            count += i
+
+    print(count)
+
+if a > b:
+    for i in range(b, a+1):
+        if i % 5 == 0:
+            count += i
+
+    print(count)
