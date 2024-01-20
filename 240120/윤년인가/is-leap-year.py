00948231db7b99466b9f6d34147ec88e85bc8d0c
growbori <156386929+growbori@users.noreply.github.com>
2024-01-20 22:07:49 +0900

    Title: 윤년인가, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240120/윤년인가/is-leap-year.py
+++ 240120/윤년인가/is-leap-year.py
@@ -1,11 +1,12 @@
 n = int(input())
 
+if n % 4 == 0:
+    print('true')
 
-if n % 4 ==0 and n % 100 == 0:
+elif n % 4 ==0 and n % 100 == 0:
     print('false')
-elif n % 4 == 0 and n % 100 == 0 and n % 400 == 0:
+elif n % 4 == 0 and n % 100 == 0 or n % 400 == 0:
     print('true')
-elif n % 4 == 0:
-    print('true')
+
 else:
     print('false')
