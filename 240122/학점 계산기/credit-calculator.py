082a887f233f04d35f65879ae478e71f4505257e
growbori <156386929+growbori@users.noreply.github.com>
2024-01-22 21:51:37 +0900

    Title: 학점 계산기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240122/학점 계산기/credit-calculator.py
@@ -0,0 +1,17 @@
+n = int(input())
+sum_val = 0
+for i in range(n):
+    arr = list(map(float, input().split()))
+    a = len(arr)
+    for j in range(a):
+        sum_val += arr[j]
+        
+    div_val = sum_val / n
+    print('% 0.1f' %div_val)
+
+    if div_val >= 4.0:
+        print('Perfect')
+    elif div_val >= 3.0 :
+        print('Good')
+    else :
+        print('Poor')
