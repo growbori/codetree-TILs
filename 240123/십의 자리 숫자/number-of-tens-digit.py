5941a0f0447a6501a9145b509684fe1dfae78d68
growbori <156386929+growbori@users.noreply.github.com>
2024-01-23 16:16:23 +0900

    Title: 십의 자리 숫자, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/십의 자리 숫자/number-of-tens-digit.py
+++ 240123/십의 자리 숫자/number-of-tens-digit.py
@@ -11,24 +11,27 @@ cnt8 = 0
 cnt9 = 0
 
 for i in range(len(arr)):
-    if arr[i] // 10 == 1:
-        cnt1 += 1
-    elif arr[i] // 10 == 2:
-        cnt2 += 1
-    elif arr[i] // 10 == 3:
-        cnt3 += 1
-    elif arr[i] // 10 == 4:
-        cnt4 += 1
-    elif arr[i] // 10 == 5:
-        cnt5 += 1
-    elif arr[i] // 10 == 6:
-        cnt6 += 1
-    elif arr[i] // 10 == 7:
-        cnt7 += 1
-    elif arr[i] // 10 == 8:
-        cnt8 += 1
-    elif arr[i] // 10 == 9:
-        cnt9 += 1
+    if arr[i] == 0:
+        break
+    else: 
+        if arr[i] // 10 == 1:
+            cnt1 += 1
+        elif arr[i] // 10 == 2:
+            cnt2 += 1
+        elif arr[i] // 10 == 3:
+            cnt3 += 1
+        elif arr[i] // 10 == 4:
+            cnt4 += 1
+        elif arr[i] // 10 == 5:
+            cnt5 += 1
+        elif arr[i] // 10 == 6:
+            cnt6 += 1
+        elif arr[i] // 10 == 7:
+            cnt7 += 1
+        elif arr[i] // 10 == 8:
+            cnt8 += 1
+        elif arr[i] // 10 == 9:
+            cnt9 += 1
 
 print(f'{1} - {cnt1}')
 print(f'{2} - {cnt2}')
