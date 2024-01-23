f75efb0824069d4d24a4c49b2dbed6bf684694b9
growbori <156386929+growbori@users.noreply.github.com>
2024-01-23 15:39:26 +0900

    Title: 전항의 두 배, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/전항의 두 배/twice-the-previous.py
+++ 240123/전항의 두 배/twice-the-previous.py
@@ -8,3 +8,13 @@ for i in range(2, 10):
 
 sentence = ' '.join(map(str, arr))
 print(sentence)
+
+# 리스트를 문자열로 변환해서 결합하고자 할 땐, .join()함수를 사용하면 된다.
+
+# 또한 만약 
+
+# sentence = ' '.join(arr) 그냥 이렇게 작성해서
+
+# TypeError: sequence item 0: expected str, int found 가 발생할 경우
+
+# sentence = ' '.join(map(str, arr)) 이렇게 문장에 map을 추가하면 된다.
