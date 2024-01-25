878182d152291b6f3e6702abc2645a155d3b845c
growbori <156386929+growbori@users.noreply.github.com>
2024-01-25 22:46:56 +0900

    Title: 특정 원소들의 합, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240125/특정 원소들의 합/sum-of-specific-elements.py
+++ 240125/특정 원소들의 합/sum-of-specific-elements.py
@@ -5,6 +5,9 @@ for i in range(4):
     arr2.append(arr)
 
 
-summ = arr2[0][0] + arr2[1][0] + arr2[1][1] + arr2[2][0] + arr2[2][1] +arr2[2][2] +  arr2[3][0] + arr2[3][1] + arr2[3][2] + arr2[3][3]
+sum_val = 0
 
-print(summ)
+for i in range(4):
+    for j in range(i + 1):
+        sum_val += arr2[i][j]
+print(sum_val)
