6b57908330142621122233c8ba3e8b8864734f89
growbori <156386929+growbori@users.noreply.github.com>
2024-01-25 17:00:30 +0900

    Title: 특정 위치의 문자, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240125/특정 위치의 문자/char-in-specific-location.py
@@ -0,0 +1,21 @@
+sentence = 'LEBROS'
+
+idx = -1
+
+for i in range(len(sentence)):
+    if sentence[i] == 'L':
+        idx = i
+    elif sentence[i] == 'E':
+        idx = i
+    elif sentence[i] == 'B':
+        idx = i
+    elif sentence[i] == 'R':
+        idx = i
+    elif sentence[i] == 'O':
+        idx = i
+    elif sentence[i] == 'S':
+        idx = i
+if idx == -1:
+    print('None')
+else:
+    print(idx)
