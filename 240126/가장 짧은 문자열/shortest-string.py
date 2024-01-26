85cdb505642b4fec74f776bbb74da22f23dd32c0
growbori <156386929+growbori@users.noreply.github.com>
2024-01-26 16:23:11 +0900

    Title: 가장 짧은 문자열, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240126/가장 짧은 문자열/shortest-string.py
+++ 240126/가장 짧은 문자열/shortest-string.py
@@ -2,23 +2,20 @@ str_1 = input()
 str_2 = input()
 str_3 = input()
 
-if len(str_1) < len(str_2) < len(str_3):
+if len(str_1) <= len(str_2) <= len(str_3):
     print(len(str_3) - len(str_1))
 
-elif len(str_1) < len(str_3) < len(str_2):
+elif len(str_1) <= len(str_3) <= len(str_2):
     print(len(str_2) - len(str_1))
 
-elif len(str_2) < len(str_1) < len(str_3):
+elif len(str_2) <= len(str_1) <= len(str_3):
     print(len(str_3) - len(str_2))
 
-elif len(str_2) < len(str_3) < len(str_1):
+elif len(str_2) <= len(str_3) <= len(str_1):
     print(len(str_1) - len(str_2))
 
-elif len(str_3) < len(str_1) < len(str_2):
+elif len(str_3) <= len(str_1) <= len(str_2):
     print(len(str_2) - len(str_3))
 
-elif len(str_3) < len(str_2) < len(str_1):
+elif len(str_3) <= len(str_2) <= len(str_1):
     print(len(str_1) - len(str_3))
-
-elif len(str_1) == len(str_2) == len(str_3):
-    print(len(str_1))
