269dcf0d99871a979bdcc7913a887eb195968c5d
growbori <156386929+growbori@users.noreply.github.com>
2024-01-26 16:21:35 +0900

    Title: 가장 짧은 문자열, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240126/가장 짧은 문자열/shortest-string.py
+++ 240126/가장 짧은 문자열/shortest-string.py
@@ -4,13 +4,18 @@ str_3 = input()
 
 if len(str_1) < len(str_2) < len(str_3):
     print(len(str_3) - len(str_1))
+
 elif len(str_1) < len(str_3) < len(str_2):
     print(len(str_2) - len(str_1))
+
 elif len(str_2) < len(str_1) < len(str_3):
     print(len(str_3) - len(str_2))
+
 elif len(str_2) < len(str_3) < len(str_1):
     print(len(str_1) - len(str_2))
+
 elif len(str_3) < len(str_1) < len(str_2):
     print(len(str_2) - len(str_3))
+
 elif len(str_3) < len(str_2) < len(str_1):
-    print(len(str_3) - len(str_1))
+    print(len(str_1) - len(str_3))
