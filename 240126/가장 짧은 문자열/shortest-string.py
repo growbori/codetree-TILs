dcde01a25c19fd1d1edf71169075f398034e2624
growbori <156386929+growbori@users.noreply.github.com>
2024-01-26 16:22:46 +0900

    Title: 가장 짧은 문자열, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240126/가장 짧은 문자열/shortest-string.py
+++ 240126/가장 짧은 문자열/shortest-string.py
@@ -19,3 +19,6 @@ elif len(str_3) < len(str_1) < len(str_2):
 
 elif len(str_3) < len(str_2) < len(str_1):
     print(len(str_1) - len(str_3))
+
+elif len(str_1) == len(str_2) == len(str_3):
+    print(len(str_1))
