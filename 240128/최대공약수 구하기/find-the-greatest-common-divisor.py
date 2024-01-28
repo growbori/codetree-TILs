20f5f69b7693f474bccb773c65c2332396da66a2
growbori <156386929+growbori@users.noreply.github.com>
2024-01-28 12:47:07 +0900

    Title: 최대공약수 구하기, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

--- 240128/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 240128/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 max_list = []
 def max_div():
     global max_list
-    for i in range(1, m):
+    for i in range(1, m+1):
         if n % i == 0 and m % i == 0: 
             max_list.append(i)
             i += 1
