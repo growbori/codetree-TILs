f643c09abf9a2634c2e716723c1f81fca23a5df5
growbori <156386929+growbori@users.noreply.github.com>
2024-01-30 23:07:13 +0900

    Title: 모이자, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240130/모이자/gather.py
+++ 240130/모이자/gather.py
@@ -1,11 +1,11 @@
 n = int(input())
-home = list(input().split())
+arr = list(input().split())
 
 result = []
-for i in range(len(home)):
+for i in range(len(arr)):
     k = 0
-    for j in range(len(home)):
+    for j in range(len(arr)):
         if j != i:
-            k += int(home[j]) * abs(j - i)
+            k += int(arr[j]) * abs(j - i)
     result.append(k)
 print(min(result))
