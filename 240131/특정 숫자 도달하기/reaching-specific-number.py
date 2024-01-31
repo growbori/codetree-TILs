d87fc4d49eb2d6f392c709a554857dd5995df50c
growbori <156386929+growbori@users.noreply.github.com>
2024-01-31 22:54:46 +0900

    Title: 특정 숫자 도달하기, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/특정 숫자 도달하기/reaching-specific-number.py
+++ 240131/특정 숫자 도달하기/reaching-specific-number.py
@@ -4,8 +4,11 @@ arr_list = []
 for i in range(len(arr)):
     arr_list.append(arr[i])
     if arr[i] >= 250:
+        arr_list.pop()
         break
-arr_list.pop() 
+    else:
+        continue
+
 sum_arr = sum(arr_list)
 div_arr = sum_arr / len(arr_list)
 print(sum_arr, '%0.1f' %div_arr)
