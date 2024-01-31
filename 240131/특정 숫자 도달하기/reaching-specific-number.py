9345887872a809204875d73a57db9fbcfe1e4f0f
growbori <156386929+growbori@users.noreply.github.com>
2024-01-31 22:48:15 +0900

    Title: 특정 숫자 도달하기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240131/특정 숫자 도달하기/reaching-specific-number.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+
+arr_list = []
+for i in range(len(arr)):
+    arr_list.append(arr[i])
+    if arr[i] >= 250:
+        break
+arr_list.pop() 
+sum_arr = sum(arr_list)
+div_arr = sum_arr / len(arr_list)
+print(sum_arr, div_arr)
