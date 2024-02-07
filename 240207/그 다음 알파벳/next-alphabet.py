5fa521d1487d600845fda7c39af2721bd3e1edbd
growbori <156386929+growbori@users.noreply.github.com>
2024-02-07 23:26:25 +0900

    Title: 그 다음 알파벳, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/그 다음 알파벳/next-alphabet.py
+++ 240207/그 다음 알파벳/next-alphabet.py
@@ -1,4 +1,12 @@
-a = input()
+a = input() # z = 122 a= 97
 
-b = ord(a) + 1
+if ord(a) == 122:
+    b = ord(a) - 25
+else:
+    for i in range(97, 122):
+        b = ord(a)+1
 print(chr(b))
+# print(chr(b))
+# print(ord(a))
+# b = ord(a)
+# print(chr(b))
