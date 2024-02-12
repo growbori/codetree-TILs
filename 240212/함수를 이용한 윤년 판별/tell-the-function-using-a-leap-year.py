84b4d5a7785e4353f8fd0316e5b9684392a8ecf3
growbori <156386929+growbori@users.noreply.github.com>
2024-02-12 17:14:08 +0900

    Title: 함수를 이용한 윤년 판별, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240212/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240212/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,12 +1,8 @@
 def is_onjeonsu(n):
-    if n % 4 == 0 and n % 100 == 0 and n % 400 == 0:
+    if n % 4==0 and n % 100 != 0:
         return True
-    elif n % 4 == 0 and n % 100 == 0:
-        return False
-    
-    elif n % 4 == 0:
-        return False
-
+    elif n % 400 == 0:
+        return True
     else:
         return False
 
