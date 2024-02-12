87dabd83810f82755ed72d49ae05b5e8a247ca5f
growbori <156386929+growbori@users.noreply.github.com>
2024-02-12 16:01:15 +0900

    Title: 함수를 이용한 369 게임, Time: 201ms, Memory: 73MB, Status: Passed - CodeTree

--- 240212/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240212/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,7 +1,14 @@
+def is_magic_number(n):
+    return n % 3 == 0 or str_number(n)
+
+def str_number(n):
+    return ('3' in str(n)) or ('6' in str(n)) or ('9' in str(n))
+
+
 a, b = map(int, input().split())
 count = 0
 for i in range(a, b+1):
-    if ('3' in str(i)) or ('6' in str(i)) or ('9' in str(i)) or i % 3 == 0:
-        count += 1 
+    if is_magic_number(i):
+        count += 1
 
 print(count)
