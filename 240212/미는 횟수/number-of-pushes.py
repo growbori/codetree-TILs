854ae036def8236f2ccbbd3dd54670e04194b480
growbori <156386929+growbori@users.noreply.github.com>
2024-02-12 15:14:45 +0900

    Title: 미는 횟수, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240212/미는 횟수/number-of-pushes.py
+++ 240212/미는 횟수/number-of-pushes.py
@@ -1,8 +1,17 @@
 a = input()
 b = input()
 
-
+count = 0
 for i in range(len(a)):
     c = a[i:] + a[:i]
     if c == b:
-        print(i)
+        count += i
+    else:
+        count += 0
+
+if count >0:
+    answer = count
+else:
+    answer = -1
+
+print(answer)
