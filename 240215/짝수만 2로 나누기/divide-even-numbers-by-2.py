9ce1500ce143f3afeb20c8ab834a261cc6b5d9e6
growbori <156386929+growbori@users.noreply.github.com>
2024-02-15 22:38:21 +0900

    Title: 짝수만 2로 나누기, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/짝수만 2로 나누기/divide-even-numbers-by-2.py
+++ 240215/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -1,11 +1,15 @@
+def div2():
+
+    for i in arr:
+        if i % 2 == 0:
+            i = i // 2
+        else:
+            i = i
+        
+        print(i, end = ' ')
+
 N = int(input())
 
 arr = list(map(int, input().split()))
 
-for i in arr:
-    if i % 2 == 0:
-        i = i // 2
-    else:
-        i = i
-
-    print(i, end = ' ')
+div2()
