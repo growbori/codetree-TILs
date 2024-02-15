8caaabc4d628c77f8ceadd475daa10e96b55535c
growbori <156386929+growbori@users.noreply.github.com>
2024-02-15 23:27:08 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240215/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -1,12 +1,18 @@
+def findword():
+    global full_txt
+    global txt
+    global c
+    for i in range(len(full_txt)):
+        if full_txt[i:i+c] == txt:
+            answer = i
+            break
+        else:
+            answer = -1
+
+    return answer
+
 full_txt = input()
 txt = input()
-
 c = len(txt)
-for i in range(len(full_txt)):
-    if full_txt[i:i+c] == txt:
-        answer = i
-        break
-    else:
-        answer = -1
 
-print(answer)
+print(findword())
