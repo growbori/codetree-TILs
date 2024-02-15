fa832356559f661f27364795b45caab82c3d482a
growbori <156386929+growbori@users.noreply.github.com>
2024-02-15 23:23:34 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240215/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -0,0 +1,12 @@
+full_txt = input()
+txt = input()
+
+c = len(txt)
+for i in range(len(full_txt)):
+    if full_txt[i:i+c+1] == txt:
+        answer = i
+        break
+    else:
+        answer = 1
+
+print(answer)
