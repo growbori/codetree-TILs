0765e16918a894684b6572332fda6262b9deb388
growbori <156386929+growbori@users.noreply.github.com>
2024-09-12 20:56:48 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/길이 단위 변환하기/change-length-unit.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    printf("9.2ft = 280.4cm");
+    printf("\n");
+    printf("1.3mi = 209214.2cm");
+    return 0;
+}
