f5adf26f6d8b8a4901d2ef4f2c7ee0ee4363c6b3
growbori <156386929+growbori@users.noreply.github.com>
2024-09-12 21:24:28 +0900

    Title: 변수 값 교체하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 2;
+    int b = 5;
+    int tem = a;
+    a = b;
+    b = tem;
+    cout << a << endl;
+    cout << b << endl;
+    return 0;
+}
