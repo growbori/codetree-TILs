9a549350d2a8729cf19699070c3073204e679216
growbori <156386929+growbori@users.noreply.github.com>
2024-09-12 22:03:20 +0900

    Title: 데이터 교환, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/데이터 교환/exchange-data.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a= 5;
+    int b = 6;
+    int c = 7;
+    cout << c << endl;
+    cout << a << endl;
+    cout << b << endl;
+    return 0;
+}
