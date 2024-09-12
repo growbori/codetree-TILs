220628f1c40cca96ba719d91b2926ab4a7093b5e
growbori <156386929+growbori@users.noreply.github.com>
2024-09-12 20:21:39 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int A = 365;
+
+int main() {
+    cout << "Total days in Year" << endl;
+    cout << A << endl;
+    printf("Circumference rate");
+    printf("\n");
+    printf("3.1415926535");
+    return 0;
+}
