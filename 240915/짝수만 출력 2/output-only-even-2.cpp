0ae94f5c70cf709670ed2cff0adce1d57016be7d
growbori <156386929+growbori@users.noreply.github.com>
2024-09-15 15:33:38 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240915/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    int i = a;
+    while ( i >= 10) {
+        if (i % 2 == 0) {
+            cout << i << " ";
+        }
+        i --;
+    }
+    return 0;
+}
