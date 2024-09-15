1cebb569349ac353e9df34583a740cff4b95709a
growbori <156386929+growbori@users.noreply.github.com>
2024-09-15 13:05:15 +0900

    Title: 삼항연산자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240915/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    string b;
+    cin >> a;
+    b = a == 100 ? "pass" : "failure";
+    cout << b << endl;
+    return 0;
+}
