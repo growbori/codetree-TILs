7d406fcde5a660040319b5cf7770756053ba565b
growbori <156386929+growbori@users.noreply.github.com>
2024-09-15 14:30:01 +0900

    Title: 남녀노소 구분짓기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240915/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d;
+    cin >> a >> b;
+    if (a == 0) {
+        if (b >= 19) {
+            cout << "MAN";
+        }
+        else {
+            cout << "BOY";
+        }
+    }
+    else {
+        if (b >= 19) {
+            cout << "WOMAN";
+        }
+        else {
+            cout << "GIRL";
+        }
+    }
+    return 0;
+}
