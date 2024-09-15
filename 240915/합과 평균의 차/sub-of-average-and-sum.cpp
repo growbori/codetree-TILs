efbb76293422a9dfaeaf6d1a4d76c262760e4fbd
growbori <156386929+growbori@users.noreply.github.com>
2024-09-15 12:40:56 +0900

    Title: 합과 평균의 차, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240915/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+    int d, e;
+    d = a+b+c;
+    e = d/3;
+    cout << d << endl << e << endl << d-e<< endl;
+    return 0;
+}
