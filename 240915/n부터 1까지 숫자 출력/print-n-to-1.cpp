b497f0c771d45b71f040a67cca12864b63b8b249
growbori <156386929+growbori@users.noreply.github.com>
2024-09-15 15:32:51 +0900

    Title: n부터 1까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240915/n부터 1까지 숫자 출력/print-n-to-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    int i;
+    i = a;
+    while (i >= 1) {
+        cout << i << " ";
+        i --;
+    }
+    return 0;
+}
