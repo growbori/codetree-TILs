4d789e97394c63d48577c9e78d8715626f2c3d27
growbori <156386929+growbori@users.noreply.github.com>
2024-09-15 13:48:01 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240915/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e;
+    f = (a> b) ? 1 : 0;
+    cout  << f << endl;
+    f = (a > c) ? 1 : 0;
+    cout  << f << endl;
+    f = (a > d) ? 1 : 0;
+    cout  << f << endl;
+    f = (a > e) ? 1 : 0;
+    cout  << f << endl;
+    return 0;
+}
