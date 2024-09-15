46bf97fed2461ee21ee24e883b9947ce743a48a4
growbori <156386929+growbori@users.noreply.github.com>
2024-09-15 12:38:44 +0900

    Title: 합과 평균, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240915/합과 평균/sum-and-avg.cpp
+++ 240915/합과 평균/sum-and-avg.cpp
@@ -5,6 +5,8 @@ int main() {
     // 여기에 코드를 작성해주세요.
     double a, b;
     cin >> a >> b;
-    cout << (a+b)  << " " << (a+b) / 2 << endl;
+    cout << fixed;
+    cout.precision(1);
+    cout << (int)(a+b)  << " " << (a+b) / 2 << endl;
     return 0;
 }
