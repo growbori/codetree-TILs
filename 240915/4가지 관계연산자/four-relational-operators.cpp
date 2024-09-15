555dfee45390ff6c0daef235d6a49db0977bb1d2
growbori <156386929+growbori@users.noreply.github.com>
2024-09-15 13:44:37 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240915/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b;
+    c = (a >= b) ? 1: 0;
+    cout << c << endl;
+    c = (a > b) ? 1 : 0;
+    cout << c << endl;
+    c = (b >= a) ? 1 : 0;
+    cout << c << endl;
+    c = (b> a) ? 1 : 0;
+    cout  << c<< endl;
+    
+    return 0;
+}
