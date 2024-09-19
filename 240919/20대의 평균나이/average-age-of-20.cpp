0efd9f3501e2b229fb376ebcfdd0f9e0fc8647a8
growbori <156386929+growbori@users.noreply.github.com>
2024-09-19 15:31:16 +0900

    Title: 20대의 평균나이, Time: 1098ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240919/20대의 평균나이/average-age-of-20.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    float sum = 0;
+    int cnt = 0;
+    while (true) {
+        int a;
+        cin >> a;
+
+        if (a >= 30) {
+            cout << fixed;
+            cout.precision(2);
+            cout << sum/cnt << endl;
+            break;
+        }
+        else {
+            sum += a;
+            cnt += 1;
+        }
+    }
+    return 0;
+}
