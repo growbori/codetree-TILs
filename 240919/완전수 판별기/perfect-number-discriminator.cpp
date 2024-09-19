d0095ac6ce3fa3b7ef538658458b75be30062607
growbori <156386929+growbori@users.noreply.github.com>
2024-09-19 13:54:32 +0900

    Title: 완전수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240919/완전수 판별기/perfect-number-discriminator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 1; i < n ; i ++) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+    if (n == sum) {
+        cout << "P";
+    }
+    else {
+        cout << "N";
+    }
+    return 0;
+}
