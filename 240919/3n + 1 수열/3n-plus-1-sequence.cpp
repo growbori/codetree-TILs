1ef01715b880ff4d9169b6a7741542ee8437a242
growbori <156386929+growbori@users.noreply.github.com>
2024-09-19 15:34:51 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240919/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int cnt = 0;
+    while (true) {
+        if (n == 1) {
+            cout << cnt;
+            break;
+        }
+        else if (n%2 == 0) {
+            n = n /2;
+            cnt ++;
+        }
+        else if (n% 2 == 1) {
+            n = 3 * n + 1;
+            cnt ++;
+        }
+    }
+
+    return 0;
+}
