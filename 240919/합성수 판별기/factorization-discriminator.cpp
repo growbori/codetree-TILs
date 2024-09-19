ab39ad5b80cb6d2a9efd57acc06c770c5f23b4fc
growbori <156386929+growbori@users.noreply.github.com>
2024-09-19 15:50:43 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240919/합성수 판별기/factorization-discriminator.cpp
+++ 240919/합성수 판별기/factorization-discriminator.cpp
@@ -5,8 +5,8 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     cin >> n;
-    int cnt;
-    for (int i = 2; i <= n; i ++) {
+    int cnt = 0;
+    for (int i = 2; i < (n-1); i ++) {
         if (n % i == 0) {
             cnt ++;
         }
