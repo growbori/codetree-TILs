e07245000631c0dcfa48b97d312cdeb7449fe547
growbori <156386929+growbori@users.noreply.github.com>
2024-09-19 15:18:41 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240919/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    while (true) {
+        int i;
+        cin >> i;
+        
+        if (i == 0) {
+            break;
+        }
+        else {
+            cout << i <<endl;
+        }
+    }
+
+    return 0;
+}
