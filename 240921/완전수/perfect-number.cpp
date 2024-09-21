e6e94a262f1de439bb20edbce394ed9fedfc488b
growbori <156386929+growbori@users.noreply.github.com>
2024-09-21 21:28:14 +0900

    Title: 완전수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240921/완전수/perfect-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+    int cnt = 0;
+    for (int i = a; i <= b; i ++) {
+        int sum = 0;
+        for (int j = 1; j < i; j ++) {
+            if (i % j == 0) {
+                sum += j;
+            }
+        }
+        if (sum == i) {
+            cnt += 1;
+        }      
+    }
+    cout << cnt;
+    return 0;
+}
