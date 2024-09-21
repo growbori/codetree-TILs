c9cf361b6f874acf12e8277f404c7a32216cccf5
growbori <156386929+growbori@users.noreply.github.com>
2024-09-22 00:45:39 +0900

    Title: 전항의 두 배, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240922/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a, b;
+    cin >> a >> b;
+    int arr[10];
+    arr[0] = a;
+    arr[1] = b;
+    for (int i = 2; i < 10; i ++) {
+        arr[i] = arr[i-1] + 2 *(arr[i-2]);
+    }
+    for (int j = 0; j < 10; j ++) {
+        cout << arr[j] << " ";
+    }
+    return 0;
+}
