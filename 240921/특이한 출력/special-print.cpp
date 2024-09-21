55fe26a6426526e5fb15a47cc3ef9009b2c630f6
growbori <156386929+growbori@users.noreply.github.com>
2024-09-21 20:44:32 +0900

    Title: 특이한 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240921/특이한 출력/special-print.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    for (int i = 1; i <=n; i ++) {
+        for (int j = 1; j <= n; j ++) {
+            if ((i +j) % 4 == 0) {
+
+                cout << "(" << i << ", " << j << ")";
+                cout << " ";
+                cout << endl;
+
+            }
+            else {
+                cout << "(" << i << ", " << j << ")";
+                cout << " ";
+            }
+            
+        }
+    }
+    return 0;
+}
