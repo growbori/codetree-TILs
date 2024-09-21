7e90668b42626ed55e286a44ac42b2d9a6d4497e
growbori <156386929+growbori@users.noreply.github.com>
2024-09-21 18:24:23 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240921/체크 다이아몬드/check-diamond.cpp
+++ 240921/체크 다이아몬드/check-diamond.cpp
@@ -1,43 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n;
     cin >> n;
 
-    // 위쪽 반쪽
-    for (int i = 0; i < n; i++) {
-        // 왼쪽 공백 출력
-        for (int j = 0; j < n - i - 1; j++) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n; j > i + 1; j--)
             cout << " ";
-        }
-
-        // 별 출력
-        for (int k = 0; k < 2 * i + 1; k++) {
-            if (k % 2 == 0)
-                cout << "*";
-            else
-                cout << " ";
-        }
+        for (int k = 0; k < i + 1; k++)
+            cout << "*" << " ";
         cout << endl;
     }
-
-    // 아래쪽 반쪽
-    for (int i = n - 2; i >= 0; i--) {
-        // 왼쪽 공백 출력
-        for (int j = 0; j < n - i - 1; j++) {
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
             cout << " ";
-        }
-
-        // 별 출력
-        for (int k = 0; k < 2 * i + 1; k++) {
-            if (k % 2 == 0)
-                cout << "*";
-            else
-                cout << " ";
-        }
+        for (int k = n; k > i; k--)
+            cout << "*" << " ";
         cout << endl;
     }
-
-    return 0;
 }
