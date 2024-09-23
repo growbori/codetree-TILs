b47181081c86e8d5a78aa2f87093c36795a6e034
growbori <156386929+growbori@users.noreply.github.com>
2024-09-23 12:33:24 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 160ms, Memory: 35MB, Status: Passed - CodeTree

--- 240923/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240923/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -2,19 +2,16 @@ n = int(input())
 
 arr = list(map(int, input().split()))
 
-profit = 0
-min_cost  = min(arr)
-c = 0
-for i in range(n):
-    if arr[i] == min_cost:
-        c = i
 
 check = []
-for j in range(c, n):
-    check.append(arr[j])
+for i in range(n):
+    for j in range(i, n):
+        front = arr[i]
+        back = arr[j]
+        ans = back - front
+        check.append(ans)
 
-if len(check) == 1:
-    print(0)
+if max(check) > 0:
+    print(max(check))
 else :
-    max_cost = max(check)
-    print(max_cost - min_cost)
+    print(0)
