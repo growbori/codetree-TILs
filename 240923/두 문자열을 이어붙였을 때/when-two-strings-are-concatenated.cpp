15c82045ff39637e3511f6dd236588fa433b5165
growbori <156386929+growbori@users.noreply.github.com>
2024-09-23 18:46:29 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240923/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a, b;
+    cin >> a >> b;
+    string c, d;
+    c = a + b;
+    d = b + a;
+    if (c == d) {
+        cout << "true";
+    }
+    else {
+        cout << "false";
+    }
+    return 0;
+}
