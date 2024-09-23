88b4d93b6b3f8238fd0b3470cd3ef44e4e1f0832
growbori <156386929+growbori@users.noreply.github.com>
2024-09-23 13:00:48 +0900

    Title: 대문자로 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240923/대문자로 바꾸기/change-to-capital.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[5][3];
+    for (int i = 0; i < 5; i ++) {
+        for (int j = 0; j < 3; j ++) {
+            cin >> arr[i][j];
+        }
+    }
+    
+    for (int i = 0; i < 5; i ++) {
+        for (int j = 0; j < 3; j ++) {
+            cout << char(int(arr[i][j]) - 32) << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
