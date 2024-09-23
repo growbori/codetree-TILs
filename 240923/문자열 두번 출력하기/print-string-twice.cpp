259403ef1b196e73476fac94f738a66ab7be4286
growbori <156386929+growbori@users.noreply.github.com>
2024-09-23 16:06:31 +0900

    Title: 문자열 두번 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240923/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string word;
+    cin >> word;
+    cout << word << endl;
+    cout << word;
+    return 0;
+}
