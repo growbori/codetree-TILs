4a013fcbdade5b95bde824bf7bcf667389154fb8
growbori <156386929+growbori@users.noreply.github.com>
2024-09-23 18:30:22 +0900

    Title: Run Length 인코딩, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 240923/Run Length 인코딩/run-length-encoding.py
+++ 240923/Run Length 인코딩/run-length-encoding.py
@@ -1,30 +1,26 @@
 word = input()
 ans = []
-count = 0
-for i in range(len(word)-1):
-    if (word[i] == word[i+1]) and (i != len(word)- 2):
-        check = word[i]
-        count += 1
+count = 1  # 반복되는 문자를 세기 위한 카운트
 
-    elif word[i] !=word[i+1] :
-        ans.append(check)
-        check = word[i+1]
-        ans.append(count+1)
-        count = 0
-    else :
-        check = word[i]
+for i in range(len(word) - 1):
+    if word[i] == word[i + 1]:
         count += 1
-        ans.append(check)
-        ans.append(count +1)
+    else:
+        ans.append(word[i])
+        ans.append(count)
+        count = 1  # 새로운 문자가 시작되므로 카운트를 1로 초기화
 
+# 마지막 문자와 그 개수를 처리
+ans.append(word[-1])
+ans.append(count)
+
+# 압축된 결과의 길이 계산
 total = 0
 for i in range(len(ans)):
-    if len(str(ans[i])) == 1:
-        total += 1;
-    elif len(str(ans[i])) == 2:
-        total += 2;
+    total += len(str(ans[i]))
 
 print(total)
 
+# 압축된 문자열 출력
 for i in range(len(ans)):
-    print(ans[i], end = '')
+    print(ans[i], end='')
