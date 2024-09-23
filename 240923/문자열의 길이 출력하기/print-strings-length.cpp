5b043386886969c6c4189ecd92ede2964306e1d6
growbori <156386929+growbori@users.noreply.github.com>
2024-09-23 16:08:00 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240923/문자열의 길이 출력하기/print-strings-length.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string first;
+    string second;
+    cin >> first >> second;
+    cout << first.length() + second.length();
+    return 0;
+}
