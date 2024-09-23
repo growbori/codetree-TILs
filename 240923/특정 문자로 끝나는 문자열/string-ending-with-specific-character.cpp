c8ea25a49eaff01931c7abfd01723c4af2ce9bed
growbori <156386929+growbori@users.noreply.github.com>
2024-09-23 17:43:07 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240923/특정 문자로 끝나는 문자열/string-ending-with-specific-character.cpp
+++ 240923/특정 문자로 끝나는 문자열/string-ending-with-specific-character.cpp
@@ -10,10 +10,15 @@ int main() {
     }
     char c;
     cin >> c;
+    int cnt = 0;
     for (int i = 0; i < 10; i ++) {
         if (arr[i][arr[i].length()-1] == c) {
             cout << arr[i] << endl;
+            cnt ++;
         }
+    }
+    if (cnt == 0) {
+        cout << "None";
     }
 
     return 0;
