a5c6d6ff46996604f53039e0ed1706ae7cfbd3ab
growbori <156386929+growbori@users.noreply.github.com>
2024-10-08 21:23:28 +0900

    Title: 고대 문명 유적 탐사, Time: 153ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241008/고대 문명 유적 탐사/ancient-ruin-exploration.py
+++ 241008/고대 문명 유적 탐사/ancient-ruin-exploration.py
@@ -6,27 +6,26 @@ def rotate(arr, si, sj):
     narr = [x[:] for x in arr]
     for i in range(3):
         for j in range(3):
-            narr[si + i][sj + j] = arr[si + 3 - 1 - j][sj + i]
+            narr[si+i][sj + j]= arr[si+3-1-j][sj+i]
     return narr
 
 def bfs(arr, v, si, sj, clr):
     q = []
-    pset = set()
     cnt = 0
+    pset = set()
     q.append((si, sj))
     pset.add((si, sj))
     v[si][sj] = 1
     cnt += 1
     while q:
         ci, cj = q.pop(0)
-        for di, dj in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+        for di, dj in ((0, 1), (0, -1), (1, 0), (-1, 0)):
             ni, nj = ci + di, cj + dj
             if 0 <= ni < 5 and 0 <= nj < 5 and v[ni][nj] == 0 and arr[ci][cj] == arr[ni][nj]:
-                cnt += 1
                 q.append((ni, nj))
                 pset.add((ni, nj))
                 v[ni][nj] = 1
-
+                cnt += 1
     if cnt >= 3:
         if clr == 1:
             for i, j in pset:
@@ -34,8 +33,7 @@ def bfs(arr, v, si, sj, clr):
         return cnt
     else:
         return 0
-
-def count_arr(arr, clr):
+def find_you(arr, clr):
     cnt = 0
     v = [[0] * 5 for _ in range(5)]
     for i in range(5):
@@ -47,30 +45,33 @@ def count_arr(arr, clr):
 
 for _ in range(K):
     mx_cnt = 0
+    narr = [x[:] for x in arr]
     for rot in range(1, 4):
         for sj in range(3):
             for si in range(3):
-                narr = [x[:] for x in arr]
                 for _ in range(rot):
-                    narr = rotate(narr, si, sj)
-
-                t = count_arr(narr, 0)
+                    narr = rotate(arr, si, sj)
+                t = find_you(narr, 0)
                 if mx_cnt < t:
                     mx_cnt = t
                     marr = narr
     if mx_cnt == 0:
-        break
+        break   # 유물 획득 불가능 시 종료
 
     cnt = 0
     arr = marr
+
     while True:
-        t = count_arr(arr, 1)
+        t = find_you(arr, 1)
         if t == 0:
             break
         cnt += t
+
         for j in range(5):
             for i in range(4, -1, -1):
                 if arr[i][j] == 0:
                     arr[i][j] = lst.pop(0)
+
     ans.append(cnt)
+
 print(*ans)
