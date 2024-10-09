5ec87e4850054822efde47e15e0f3b397e3208ed
growbori <156386929+growbori@users.noreply.github.com>
2024-10-09 13:48:29 +0900

    Title: 마법의 숲 탐색, Time: 380ms, Memory: 29MB, Status: Passed - CodeTree

--- 241009/마법의 숲 탐색/magical-forest-exploration.py
+++ 241009/마법의 숲 탐색/magical-forest-exploration.py
@@ -15,13 +15,13 @@ def bfs(si, sj):
     mx_h = 0
     while q:
         ci, cj = q.pop(0)
-        mx_h = max(mx_h, ci)
+        mx_h = max(mx_h, ci)    # ci로 내려갈 수 있는 최대를 구하기!
         for di, dj in ((0, 1), (1, 0), (0, -1), (-1, 0)):
             ni, nj = ci + di, cj + dj
             if v[ni][nj] == 0 and ((arr[ni][nj] == arr[ci][cj]) or ((ci, cj) in exit_set and arr[ni][nj] > 1)):
                 q.append((ni, nj))
                 v[ni][nj] = 1
-    return mx_h - 2
+    return mx_h - 2 # ci가 갈 수 없는 0, R 부분의 값 빼주기!
 
 ans = 0
 nums = 2
@@ -33,7 +33,7 @@ for cj, dr in units:
         elif arr[ci-1][cj-1] == arr[ci][cj-2] == arr[ci+1][cj-1] == arr[ci + 1][cj-2] == arr[ci + 2][cj-1] == 0:
             ci += 1
             cj -= 1
-            dr = (dr-1) % 4
+            dr = (dr-1) % 4 # +- 조심하기!
         elif arr[ci-1][cj+1] == arr[ci][cj+2] == arr[ci+1][cj+1] == arr[ci+1][cj+2] == arr[ci+2][cj+1] == 0:
             ci += 1
             cj += 1
@@ -46,9 +46,9 @@ for cj, dr in units:
         exit_set = set()
     else:
         arr[ci-1][cj] = arr[ci + 1][cj] = nums
-        arr[ci][cj-1:cj+2] = [nums] * 3
+        arr[ci][cj-1:cj+2] = [nums] * 3 # 리스트 형태로 받는거 주의하기!
         nums += 1
-        exit_set.add((ci + di[dr], cj + dj[dr]))
+        exit_set.add((ci + di[dr], cj + dj[dr])) # 출구쪽 좌표 정리해놓기!
         ans += bfs(ci, cj)
 
 print(ans)
